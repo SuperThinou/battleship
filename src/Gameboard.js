df1b37404fe818b8648b7794febedfd5d8ab6b3e
NathanBernard <nbx-off@outlook.fr>
2026-05-26 10:53:47 +0200

    add shipTypes object table

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -3,4 +3,13 @@ export default class Gamboard {
     this.x = 10;
     this.y = 10;
   }
+  placeShip() {
+    const shipTypes = [
+      { name: "Carrier", length: 5 },
+      { name: "Battleship", length: 4 },
+      { name: "Destroyer", length: 3 },
+      { name: "Submarine", length: 3 },
+      { name: "Patrol Boat", length: 2 },
+    ];
+  }
 }
